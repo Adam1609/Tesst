94ad2a83119fb898d17f29884b927550207a48cb
PeopleMessage <anhratthichquay123@yahoo.com.vn>
2016-07-18 12:06:38 +0700

    Update Study.php

--- src/Study/Study.php
+++ src/Study/Study.php
@@ -3,22 +3,36 @@
 namespace Study;
 
 use pocketmine\Player;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
+use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\utils\TextFormat as C;
 use pocketmine\utils\Config;
 
 class Study extends PluginBase implements Listener
 {
   public function onLoad(){
-    $this->getLogger()->info(C::RED . "School is openning");
+    $this->getLogger()->info(C::AQUA . "School is openning");
   }
   public function onEnable(){
-    $this->getLogger()->info(C::GREEN . "Ready to Study on version" . $this->getDescription()->getVersion());
+    @mkdir($this->getDataFolder());
+    $this->classs = new Config($this->getDataFolder() . "classs.yml", Config::YAML);
+    $this->getLogger()->info(C::GREEN . "Ready to Study on version " . $this->getDescription()->getVersion());
     $this->saveDataConfig();
   }
     
   public function onDisable(){
-    $this->getLogger()->info(C::AQUA . "School closed");
+    $this->classs->save();
+    $this->getLogger()->info(C:RED . "School closed");
+  }
+  
+  public function onJoin(PlayerJoinEvent $event){
+    $stu = $event->getPlayer();
+    $player = $event->getPlayer()->getName();
+    if ($this->classs->exists($Player)){
+      $stu->setDisplayName(TextFormat::LIGHT_PURPLE . "[ " .$   "] " . $stu->getDisplayName());
+    }
   }
 }
