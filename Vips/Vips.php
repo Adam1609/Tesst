5a39dde01be553b3fb1675323434b816cf2e6853
PeopleMessage <anhratthichquay123@yahoo.com.vn>
2016-07-21 22:38:34 +0700

    Create Vips.php

---
+++ Vips/Vips.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Vips;
+
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+
+class Vips extends PluginBase{
+  /About Plugin Const
+	
+	/** @var string PRODUCER Plugin producer */
+	const PRODUCER = "Godly";
+	
+	/** @var string MAIN_WEBSITE Plugin producer website */
+	const MAIN_WEBSITE = "http://weelux.com";
+	
+	//Other Const
+	
+	/** @var string PREFIX Plugin prefix */
+	const PREFIX = "&b[&aCustom&cAlerts&b] ";
