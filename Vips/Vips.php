3d7dc9ea4e371235ee645c5ae999c5233b8427df
PeopleMessage <anhratthichquay123@yahoo.com.vn>
2016-07-22 22:23:31 +0700

    Update Vips.php

--- Vips/Vips.php
+++ Vips/Vips.php
@@ -2,55 +2,101 @@
 
 namespace Vips;
 
-use pocketmine\Player;
-use pocketmine\plugin\PluginBase;
-use pocketmine\Server;
-use pocketmine\utils\Config;
-use pocketmine\utils\TextFormat as C;
+use battlekits\command\BattleKitCommand;
+use battlekits\economy\EconomyLoader;
+use battlekits\economy\BaseEconomy;
+use battlekits\kit\KitHistoryStore;
+use battlekits\kit\KitPaymentController;
+use Vips\vips\KitStore;
+use battlekits\sign\SignListener;
 use pocketmine\item\Item;
 
 class Vips extends PluginBase{
-  //About Plugin Const
-	
-	/** @var string PRODUCER Plugin producer */
-	const PRODUCER = "Godly";
-	
-	/** @var string MAIN_WEBSITE Plugin producer website */
-	const MAIN_WEBSITE = "http://weelux.com";
-	
-	//Other Const
-	
-	/** @var string PREFIX Plugin prefix */
-	const PREFIX = "&b[&aVI&cPs&b] ";
+  /** @var  EconomyLoader */
+    private $economyLoader;
+    /** @var  BaseEconomy */
+    private $economy = false;
+    /** @var  vipsStore */
+    private $vipsStore;
+    /** @var  vipsCommand */
+    private $mainCommand;
+    /** @var  vipsPaymentController */
+    private $vipsPaymentController;
+    /** @var  SignListener */
+    private $signVips;
+    /** @var  KitHistoryStore */
+    private $vipsHistoryStore;
     public function onEnable(){
-        $this->getLogger()->info("Vips by ChienDz enabled!");
         $this->saveDefaultConfig();
-        $this->saveResource("config.yml");
-    }
-    public function onDisable(){
-        $this->getLogger()->info("Vips disaled! :o");
-    }
-    public function onLoad(){
-        $this->getLogger()->info("Vips by ChienDz!");
-    }
-    public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+        $this->economyLoader = new EconomyLoader($this);
+        $this->economyLoader->load();
+        $this->vipsStore = new vipsStore($this);
+        $this->vipsHistoryStore = new VipsHistoryStore($this);
+        $this->vipsPaymentController = new VipsPaymentController($this);
         
-        switch($cmd->getName()){
-            case "vips":
-            	if $sender->hasPermission("vips.cmd")){
-            		$sender->sendMessage(C::ITALIC. C::YELLOW. "Vips Commands & Info!");
-            		$sender->sendMessage(C::ITALIC. C::AQUA. "/vips or /dnt list -> See all the vips!");
-            		if(isset($args[0])){
-                           case "list":
-                               if($sender->hasPermission("vips.cmd.list")){
-                                   $sender->sendMessage(C::ITALIC. C::LIGHT_GREEN. "--==" . C::YELLOW . "VipS" . C::LIGHT_GREEN . "==--" );
-                                   $sender->sendMessage(C::ITALIC. $this->getConfig()->get("custom_message"));
-                                   return true;
-                                   break;
-            	                }
-            		} 
-            	}
-        }  
+        $this->mainCommand = new VipsCommand($this);
+        $this->getServer()->getCommandMap()->register("vips", $this->mainCommand);
+        
+        $this->signListener = new SignListener($this);
     }
+    /**
+     * @param \vips\economy\BaseEconomy $economy
+     */
+     public function setEconomy(BaseEconomy $economy){
+        $this->economy = $economy;
+     }/**
+     * @return \vips\economy\BaseEconomy
+     */
+    public function getEconomy(){
+        return $this->economy;
+    }
+    /**
+     * @return bool
+     */
+    public function isLinkedToEconomy(){
+        return $this->economy instanceof BaseEconomy;
+    }
+    /**
+     * @return \vips\vips\VipsStore
+     */
+    public function getVipsStore(){
+        return $this->vipsStore;
+    }
+    /**
+     * @return \vips\command\VipsCommand
+     */
+    public function getMainCommand(){
+        return $this->mainCommand;
+    }
+    /**
+     * @return \vips\economy\EconomyLoader
+     */
+    public function getEconomyLoader(){
+        return $this->economyLoader;
+    }
+    /**
+     * @return \vipss\sign\SignListener
+     */
+    public function getSignListener(){
+        return $this->signListener;
+    }
+    /**
+     * @return \vips\vips\VipsPaymentController
+     */
+    public function getVipsPaymentController(){
+        return $this->vipsPaymentController;
+    }
+    /**
+     * @return \vips\vips\VipsHistoryStore
+     */
+    public function getVipsHistoryStore(){
+        return $this->kitVipstoryStore;
+    }
+    public function reportEconomyLinkError(){
+        $this->getLogger()->critical("The link to " . $this->economy->getName() . " has been lost. Paid vips are no longer available.");
+        $this->economy = false;
+    }
+}
+
     
        
